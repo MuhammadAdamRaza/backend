0ad505ec1f61555381443cd6049fbe624e10be1e
Adam1224345 <adamraza5t7@gmail.com>
2026-03-15 00:00:51 +0500

    Revert project structure and vercel.json to original functional state to debug 404s

--- api/test.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, jsonify
-app = Flask(__name__)
-
-@app.route('/api/test')
-def test():
-    return jsonify({"status": "python is working"})
-
-if __name__ == '__main__':
-    app.run()
